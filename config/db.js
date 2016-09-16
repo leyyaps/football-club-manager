e3b8e2780c7470966e0d0471c50858bac88454ba
leyyaps <andrew.smith@ipsofactouk.com>
2016-09-16 10:19:19 +0100

    change to production database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/football-club-manager-express-test",
   development: "mongodb://localhost/football-club-manager-express-app",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/football-club-manager-express-app"
+  production: process.env.MONGODB_URI || "mongodb://localhost/football-club-manager-express-app"
 }
 
 module.exports = function(env) {
